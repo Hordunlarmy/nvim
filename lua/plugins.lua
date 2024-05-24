8f02880846dd8a179b3a502a9b2b8bc057479a52
hordunlarmy <hordunlarmy@gmail.com>
2024-05-24 06:31:44 +0100

    Update dependencies and setup for nvim-cokeline plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -113,7 +113,16 @@ require("lazy").setup({
     "nvim-lualine/lualine.nvim",
 
     -- line for showing open buffers in tabline
-    {'akinsho/bufferline.nvim', version = "*", dependencies = 'kyazdani42/nvim-web-devicons'},
+    -- {'akinsho/bufferline.nvim', version = "*", dependencies = 'kyazdani42/nvim-web-devicons'},
+    {
+        "willothy/nvim-cokeline",
+        dependencies = {
+            "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
+            "nvim-tree/nvim-web-devicons", -- If you want devicons
+            "stevearc/resession.nvim"       -- Optional, for persistent history
+    	},
+    config = true
+    },
 
     -- add highlight matching color to color codes (hex, rgb, etc.)
     {"RRethy/vim-hexokinase", build = "make hexokinase"},
