553a1770208eec6a8e1776ae341078d67112e7a0
Hordunlarmy <Hordunlarmy@gmail.com>
2024-06-25 14:49:26 +0100

    refactor(linters.lua): Change linter from semistandard to prettier

--- lua/odun/linters.lua
+++ lua/odun/linters.lua
@@ -93,8 +93,8 @@ require("formatter").setup({
     javascript = {
       function()
         return {
-          exe = "semistandard",
-          args = {"-", "--fix"},
+          exe = "prettier",
+          args = {"--stdin-filepath", vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),},
           stdin = true,
           --cwd = vim.fn.expand('%:p:h'),
         }
