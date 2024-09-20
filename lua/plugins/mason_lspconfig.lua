e21593fe954d52ba1892c1249d1d5c4d03cc777a
Hordunlarmy <Hordunlarmy@gmail.com>
2024-09-21 00:13:46 +0100

    chore: Update remote

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -2,7 +2,7 @@ local opts = {
 	ensure_installed = {
 		-- "efm",          -- General purpose language server, can be used for linting and formatting
 		"bashls",             -- Language server for Bash scripts
-		"tsserver",           -- Language server for TypeScript and JavaScript
+		"ts_ls",           -- Language server for TypeScript and JavaScript
 		"jedi_language_server", -- Language server for Python (replaces pyright)
 		"lua_ls",             -- Language server for Lua
 		"emmet_ls",           -- Language server for Emmet, useful for HTML and CSS
